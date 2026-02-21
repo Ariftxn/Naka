2460f9c091656e4444ebb8b47ce56a16b8001826
Ariftxn <121342224+Ariftxn@users.noreply.github.com>
2026-02-21 16:49:48 +0700

    Update dice.js

--- commands/dice.js
+++ commands/dice.js
@@ -1,1 +1,9 @@
-.
+export default {
+  name: "dice",
+  description: "Roll dice",
+  async execute({ message, args }) {
+    const sides = parseInt(args[0]) || 6;
+    const roll = Math.floor(Math.random()*sides)+1;
+    message.channel.send({ embeds:[{ title:"Dice Roll", description:`You rolled a ${roll} (1-${sides})`, color:0x9B59B6 }] });
+  }
+};
